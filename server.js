b38226ea9684b21540be54ea978b0cb9ae85ac4b
Travis Russell <mtrussell@gmail.com>
2021-08-22 15:15:17 -0600

    added handling for request for non-existing routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // library includes
 const express = require('express');
+const path = require('path')
 // set port and express instance
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,10 +12,12 @@ const api = require('./routes/api');
 const html = require('./routes/html');
 app.use('/api', api);
 app.use('/', html);
-
-
 // serve the /public directory
 app.use(express.static('public'))
+//returns to index when a non-existing page is requested
+app.use(function (req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, './public/index.html'));
+})
 
 // start the server listening on PORT
 app.listen(PORT, () => {
